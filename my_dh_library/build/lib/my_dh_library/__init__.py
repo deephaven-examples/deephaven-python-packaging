6da05015ceb9b5288ecc181b11c99e771754a5dc
elijahpetty <elijahpetty@illumon.com>
2026-08-27 15:24:12 -0500

    gitignore file

--- my_dh_library/build/lib/my_dh_library/__init__.py
+++
@@ -1,7 +0,0 @@
-"""My Deephaven package for data processing."""
-
-__version__ = "0.1.0"
-
-from my_dh_library.queries import filter_by_threshold, add_computed_columns, summarize_by_group
-
-__all__ = ["filter_by_threshold", "add_computed_columns", "summarize_by_group"]
